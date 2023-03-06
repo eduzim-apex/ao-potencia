0c5c42a8a24287df306cf6afdba484c05cd15184
eduzim-apex <106496648+eduzim-apex@users.noreply.github.com>
2023-03-05 22:52:14 -0300

    Add files via upload

---
+++ site_potencia/footer.php
@@ -0,0 +1,5 @@
+<?php
+echo "<p>Copyright &copy; 2021-" . date("Y") . " Dev Tech</p>";
+echo '<a href="https://www.instagram.com/devs_tech1/?igshid=ZDdkNTZiNTM%3D"><img src="https://png.pngtree.com/png-vector/20221018/ourmid/pngtree-instagram-social-platform-icon-png-image_6315976.png" style="height: 30px; width: 30x;"></a>';
+echo ' <a href="https://twitter.com/Apex_eduzim"><img src="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-4.png" style="height: 30px; width: 30x; margin-right: 7%;"></a></p>'
+?>
---
+++ site_potencia/nav.php
@@ -0,0 +1,7 @@
+<?php
+
+echo '<h1 class="jumbotron-heading" style="color: white; margin-right: 140px;">Abaixo você poderá Solicitar um de nossos serviços</h1>
+<p class="lead text-muted">Selecione um de nossos serviços de seu interesse e fale com nossa equipe de Desenvolvimento</p>
+  </p>';
+
+?>
